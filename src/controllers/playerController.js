f86900776383342d5e748ad28e440db429ef5eae
InstantlyMoist <32609460+InstantlyMoist@users.noreply.github.com>
2023-05-31 14:44:45 +0200

    Fixed played adding

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,41 +1,31 @@
-let players = require('../models/player');
+let players = require("../models/player");
 
 module.exports = function (app) {
+  //Player aanmaken.
+  app.post("/player", async (req, res) => {
+    let body = req.body;
+    // Verwachte parameters:
+    // deviceId: String,
+    // name: String,
 
-    //Player aanmaken.
-    app.post('/player', async (req, res) => {
-        let body = req.body;
-        // Verwachte parameters:
-        // deviceId: String,
-        // name: String,
-
-        // Create player.
-        let player = new players({
-            deviceId: body.deviceId,
-            name: body.name,
-        });
-
-        await players.findOne({ deviceId: body.deviceId })
-            .then(async (foundPlayer) => {
-                foundPlayer.name = body.name;
-                await players.updateOne(foundPlayer);
-                await foundPlayer.save().then((savedPlayer) => {
-                    res.status(201).json(savedPlayer);
-                })
-                    .catch((err) => {
-                        console.log(err.errors);
-                        res.status(400).send(err.errors);
-                    });
-            })
-            .catch((err) => {
-                // Save player to database.
-                player.save().then((savedPlayer) => {
-                    res.status(201).json(savedPlayer);
-                })
-                    .catch((err) => {
-                        console.log(err.errors);
-                        res.status(400).send(err.errors);
-                    });
-            });
+    // Create player.
+    let player = new players({
+      deviceId: body.deviceId,
+      name: body.name,
     });
-}
+
+    await players
+      .findOneAndUpdate({ deviceId: body.deviceId }, { name: body.name })
+      .catch((err) => {
+        player
+          .save()
+          .then((savedPlayer) => {
+            res.status(201).json(savedPlayer);
+          })
+          .catch((err) => {
+            console.log(err.errors);
+            res.status(400).send(err.errors);
+          });
+      });
+  });
+};
