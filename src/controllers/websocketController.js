7d2245c3f19fe3f7f5aed3173fac0296fc7a8197
Mattijs Pronk <456410@student.fontys.nl>
2023-06-09 11:43:50 +0200

    weboscket fix

--- src/controllers/websocketController.js
+++ src/controllers/websocketController.js
@@ -24,31 +24,41 @@ module.exports = function (app) {
 
     function sendDataToClients(data) {
         let bodyJson = JSON.parse(data);
+        var j = 0;
 
         for (var i = 0; i < bodyJson.clients.length; i++) {
 
             const client = clients.get(bodyJson.clients[i].deviceId);
             if (client) {
+                j++
+                console.log('aantal gestuurd', j);
                 client.send(JSON.stringify({ teamId: bodyJson.clients[i].teamId, message: bodyJson.message }));
             }
         }
+
+        // bodyJson.clients.forEach((client) => {
+        //     const clientToSend = clients.get(client.deviceId);
+
+        //     if (clientToSend) {
+        //         clientToSend.send(JSON.stringify({ teamId: bodyJson.clients[i].teamId, message: bodyJson.message }));
+        //     }
+        //   });
     }
 
     // Store the connected clients
     const clients = new Map();
 
+    //event om een game te starten sturen naar alle spelers.
+    eventEmitter.on('startGame', (data) => {
+        sendDataToClients(data);
+    });
+
+    //event om een antwoord naar elke client te sturen in een team.
+    eventEmitter.on('correctAnswer', (data) => {
+        sendDataToClients(data);
+    });
+
     app.ws("/websocket", (ws) => {
-        // Load all packets
-
-        //event om een game te starten sturen naar alle spelers.
-        eventEmitter.on('startGame', (data) => {
-            sendDataToClients(data);
-        });
-
-        //event om een antwoord naar elke client te sturen in een team.
-        eventEmitter.on('correctAnswer', (data) => {
-            sendDataToClients(data);
-        });
 
         ws.on("message", (data) => {
             let bodyJson = JSON.parse(data);
@@ -59,7 +69,9 @@ module.exports = function (app) {
                     clients.delete(bodyJson.deviceId);
                 }
 
+                ws.clientId = bodyJson.deviceId;
                 clients.set(bodyJson.deviceId, ws);
+
                 ws.send(JSON.stringify({ deviceId: bodyJson.deviceId }));
             }
             if (bodyJson.type === 'sendlocation') {
